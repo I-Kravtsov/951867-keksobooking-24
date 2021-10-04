10bb44b0e982915aba7dc8a6d48a4bf1813899b8
i-kravtsov <rhtr@maill.ru>
2021-10-04 18:00:28 +0300

    Создал пустой шаблон объекта и заполнил первое свойство

--- js/main.js
+++ js/main.js
@@ -6,4 +6,31 @@ const getRandomNumber = (from, to, precission) => {
   return (from < to) ? getRandom(from, to, precission) : getRandom (to, from, precission);
 };
 
-getRandomNumber(5, 3, 4);
+const avatarNumber = String(getRandomNumber(0, 10)).padStart(2, 0);
+
+const createAdvertisement = () => {
+  return {
+    author: {
+      avatar: `img/avatars/user${avatarNumber}.png`,
+    },
+    offer: {
+      title: 'New title',
+      address: '',
+      price: 0,
+    },
+    type: '',
+    rooms: '',
+    guests: 0,
+    checkin: '',
+    checkout: '',
+    features: [],
+    description: '',
+    photos: [],
+    location: {
+      lat: 0,
+      lng: 0,
+    },
+  }
+}
+
+console.log(createAdvertisement());
