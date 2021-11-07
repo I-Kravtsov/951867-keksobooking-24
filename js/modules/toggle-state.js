e1e51dd51245841378bdb2318ee7edef3a9d4c36
Kravtsov Ivan <rhtr@mail.ru>
2021-11-07 03:22:41 +0300

    Сделал переключение состояния страницы

---
+++ js/modules/toggle-state.js
@@ -0,0 +1,25 @@
+const adForm = document.querySelector('.ad-form');
+const mapFilter = document.querySelector('.map__filters');
+
+const setInactivform = (form, disableClass, state) => {
+  const activeElements = form.children;
+  if (state === 'active') {
+    form.classList.remove(disableClass);
+    for (const element of activeElements) {
+      element.disabled = false;
+    }
+  } else {
+    form.classList.add(disableClass);
+    for (const element of activeElements) {
+      element.disabled = true;
+    }
+  }
+};
+
+
+const setInactivState = (state) => {
+  setInactivform(adForm, 'ad-form--disabled', state);
+  setInactivform(mapFilter, 'map__filters--disabled', state);
+};
+
+setInactivState('infactive');
